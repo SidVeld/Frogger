63799e84003f6fcb53d221448b4cd3a163d2840f
SidVeld Von Disco <sidveld@gmail.com>
2022-03-20 16:14:03 +0300

    Add newlines for list of scripts

--- frogger/__main__.py
+++ frogger/__main__.py
@@ -4,7 +4,7 @@ from frogger.controller import Controller
 controller = Controller()
 controller.load_scripts()
 for number, script in enumerate(controller.scripts):
-    print(f"{number + 1}: {script.name}\n{script.description}\nMade by:{script.author}")
+    print(f"{number + 1}: {script.name}\n{script.description}\nMade by: {script.author}\n\n")
 
 choice = int(input("?\tNumber. Which script we want to run: ")) - 1
 
